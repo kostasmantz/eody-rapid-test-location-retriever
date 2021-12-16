42525a0bb0feed6c33df1845bdf9e3048b68528a
Kostas Mantzavelas <kostiksm@gmail.com>
2021-12-16 18:46:18 +0200

    Initial commit

---
+++ src/main/java/com/mantzavelas/eodyrapidtestpoiretriever/EodyRapidTestPoiRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.mantzavelas.eodyrapidtestpoiretriever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EodyRapidTestPoiRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EodyRapidTestPoiRetrieverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mantzavelas/eodyrapidtestpoiretriever/EodyRapidTestPoiRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantzavelas.eodyrapidtestpoiretriever;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EodyRapidTestPoiRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
